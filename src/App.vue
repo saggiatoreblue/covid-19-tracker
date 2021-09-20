44b838b2cc28c7d25bd4a023071b083472bdf825
Michael Botelho <saggiatoreblue@gmail.com>
2021-09-20 08:13:11 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+      <h4 class="display-4 mb-5">COVID Data</h4>
+      <PieChart :chartData="chartData" ></PieChart>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+const apiKey = '8d0d3a74c978490aabcf2df398a15f7e';
+import moment from 'moment';
+
+import PieChart from './components/Pie';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PieChart
+  },
+
+  data() {
+    return {
+      chartData: {}
+    }
+  },
+
+  async created() {
+    try {
+      const { data } = await axios.get(`https://api.covidactnow.org/v2/country/US.json?apiKey=${apiKey}`);
+      const date = moment(data.lastUpdatedDate, 'YYYY-MM-DD').format('MMMM DD');
+
+      const actuals = data.actuals;
+
+      this.chartData = {
+        cases: actuals.positiveTests,
+        hospitals: actuals.hospitalBeds.currentUsageCovid,
+        icus: actuals.icuBeds.currentUsageCovid,
+        vaccinations: actuals.vaccinationsCompleted,
+        deaths: actuals.deaths
+      }
+
+
+    } catch(e) {
+      console.error(e);
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ #app .container {
+   height: 100vh;
+ }
 </style>
---
+++ src/components/Pie.vue
@@ -0,0 +1,48 @@
+<script>
+import { Pie } from 'vue-chartjs';
+
+export default {
+  extends: Pie,
+
+  props: {
+    chartData: {
+      type: Object
+    }
+  },
+
+  mounted() {
+
+    const cases = this.chartData.cases;
+    const deaths = this.chartData.deaths;
+    const vaccinations = this.chartData.vaccinations;
+
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+    const data = {
+      labels: [
+        'Cases',
+        'Deaths',
+        'Vaccinations'
+      ],
+      datasets: [{
+        label: 'COVID DATA',
+        data: [cases, deaths, vaccinations],
+        backgroundColor: [
+          'rgb(42,109,5)',
+          'rgb(133,18,18)',
+          'rgb(33,67,119)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+    this.renderChart(data, options);
+
+  }
+
+}
+</script>
+
+
